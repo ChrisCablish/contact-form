b48b878c9004d145cdaf8ac6d0f284b8e6423926
ChrisCablish <112437010+ChrisCablish@users.noreply.github.com>
2024-07-26 08:46:14 -0500

    import font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,20 @@
 "use client";
 
-import ContactForm from "./components/Contact";
+import Head from "next/head";
+import ContactForm from "./components/ContactForm";
 
 export default function Home() {
   return (
-    <main>
-      <ContactForm />
-    </main>
+    <>
+      <Head>
+        <style>
+          @import
+          url('https://fonts.googleapis.com/css2?family=Karla:ital,wght@0,200..800;1,200..800&family=Roboto:wght@400;700&display=swap');
+        </style>
+      </Head>
+      <main>
+        <ContactForm />
+      </main>
+    </>
   );
 }
